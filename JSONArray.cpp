7e4bec6acd94ef864e3512ec5ae059316f989993
Ezio Ballarin <eballarin@blue.cs.sonoma.edu>
2017-12-05 17:52:24 -0800

    Cleaned up JSONArray.cpp

--- JSONArray.cpp
+++ JSONArray.cpp
@@ -3,7 +3,7 @@
  *  Author: Ezio Ballarin 
  *  Student ID: 005633321
  *  Creation Date: 12-03-2017
- *  Last Modified: Tue 05 Dec 2017 05:46:16 PM PST
+ *  Last Modified: Tue 05 Dec 2017 05:52:09 PM PST
  *
  *  Description: Implementation of JSONArray class.
  *
@@ -19,12 +19,18 @@ JSONArray::JSONArray() {
 JSONArray::~JSONArray() {
     delete _listOfDataObjects;
 }
-/*
-virtual JSONDataObject *jsonObjectNode() = 0;
-int numJSONObjects();
-void parseJSONArray(std::fstream &stream);
-virtual void print();
-*/
+
+/**
+ * Name: parseJSONArray() 
+ * 
+ * Purpose: Parse a given JSON filestream for objects.
+ * Parameters: std::fstream &stream - the JSON filestream to read from.
+ * Return: None.
+ * 
+ * Notes: We expect the internal cursor of the JSON filestream to be at
+ * the beginning of the JSON array. After parsing, the internal cursor will
+ * be at the end of the array.
+ */
 void JSONArray::parseJSONArray(std::fstream &stream) {
     char c;
     std::cout << std::endl << "Parsing array..." << std::endl;
@@ -44,14 +50,43 @@ void JSONArray::parseJSONArray(std::fstream &stream) {
     }
 }
 
+/**
+ * Name: jsonObjectNode()
+ * 
+ * Purpose: Returns a pointer to a generic type of element in this
+ *          JSONArray's collection. I.e. Artists.jsonObjectNode() would
+ *          return a pointer to a new Artist() object, and so on.
+ * Parameters: None.
+ * Return: A pointer to the type of elements contained in this JSONArray.
+ * 
+ */
 JSONDataObject* JSONArray::jsonObjectNode() {
     return new JSONDataObject();
 }
 
+/**
+ * Name: numJSONObjects()
+ * 
+ * Purpose: Determine the number of JSON objects in this JSON array.
+ * Parameters: None.
+ * Return: The number of JSON objects in the internal vector.
+ * 
+ */
 int JSONArray::numJSONObjects() {
     return _listOfDataObjects->size();
 }
 
+/**
+ * Name: print()
+ * 
+ * Purpose: Debugging the objects contained in this JSON array.
+ *          Output varies in format, but generally tests the operations
+ *          of the underlying objects, along with the operations of the
+ *          JSONArray.
+ * Parameters: None.
+ * Return: None.
+ * 
+ */
 void JSONArray::print() {
     std::cout << "JSON Array contains: " << std::endl;
     for (std::vector<JSONDataObject*>::iterator it = _listOfDataObjects->begin();
